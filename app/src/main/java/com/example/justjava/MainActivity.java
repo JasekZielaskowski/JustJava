c351ebdafd16e17a56f2a4a060f1fe7b851181e8
Jasek <02jasek.zielaskowski@gulllakecs.org>
2021-03-04 12:45:28 -0500

    Unit 7 - Update String priceMessage

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -60,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
-        displayMessage(priceMessage);
+        String message = "She said\"1 dollar\"";
+        displayMessage(message);
     }
 
     /**
